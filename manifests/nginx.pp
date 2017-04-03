a1f7ae09f73071d781dc35904f670e1a05873a44
root <root@swarnaravistar1.mylabserver.com>
2017-04-03 05:02:36 +0000

    fixed

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,6 +1,12 @@
-class profile::nginx {
- include ::nginx
-
-  ::nginx::vhost { $facts['fqdn']:
- }
+class profile::nginx(
+  Array $vhosts,
+) {
+  include ::nginx
+  
+  $vhosts.each |$vhost| {
+   ::nginx::vhost { $facts['fqdn']:
+      port           => $vhost['port'],
+      server_aliases => $vhost['server_aliases'],
+    }
+  }
 }
